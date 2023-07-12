f3c6d732cc498bfed4d80cece8505d10e0fa8b7b
Ayo-234 <129855185+Ayo-234@users.noreply.github.com>
2023-07-12 10:34:12 +0100

    bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,35 +1,3 @@
-import random
-
-class Human:
-    height = 180
-    weight = 55
-
-    def sleep(self):
-        time = random.randint(1, 12)
-        print(f'Slept for {time} hours')
-
-    def walk(self):
-        walking_time = random.randint(2, 4)
-        walking_distance = random.randint(2, 8)
-        speed = walking_distance / walking_time
-        print(f'Time taken: {walking_time} hours\n'
-              f'Distance walked: {walking_distance} miles\n'
-              f'Speed: {speed} miles per hour')
-
-    def calculate_weight_change(self):
-        calories_consumed = float(input("Enter calories consumed: "))
-        calories_burnt = float(input("Enter calories burnt: "))
-        weight_change = (calories_consumed - calories_burnt) / 3500
-        self.weight += weight_change
-        print(f'Weight change: {weight_change:.2f} kg')
-
-    def track_size(self):
-        print("Size tracker")
-        class SizeSpecifics:
-            length = float(input("Enter specific height: "))
-            weight = float(input("Enter specific weight: "))
-            shoe_size = float(input("Enter specific shoe size: "))
-
 class Account:
     name = "John"
     account_number = 27767289183766
@@ -92,9 +60,8 @@ class SavingsAccount(BankAccount):
     def __init__(self, account_number, account_balance=0, interest_rate=0.01):
         super().__init__(account_number, account_balance)
         self.interest_rate = interest_rate
-
+        
     def add_interest(self):
         interest = self.account_balance * self.interest_rate
         self.account_balance += interest
         return f"${interest:.2f} interest added successfully!"
-##
